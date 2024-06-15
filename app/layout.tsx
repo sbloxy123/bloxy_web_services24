1ca8caaf07cba1ec443f41b1c8a0ae5359057d4b
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-06-15 11:29:38 +0100

    removed schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter, Mulish, Poppins } from "next/font/google";
-import { LocalBusiness, WithContext } from "schema-dts";
+// import { LocalBusiness, WithContext } from "schema-dts";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@material-tailwind/react";
@@ -16,72 +16,72 @@ const mulish = Mulish({
   variable: "--font-mulish",
 });
 
-export const metadata: Metadata = {
-  title: "Bloxy Web Services | Freelance Web Designer & Developer 👨‍💻",
-  description:
-    "🚀 Supercharge your online presence with incredibly fast websites & apps 🚀 - E-Commerce 🛍️ | Lead Gen 🎯 | Business 🏢 | + more!",
-  metadataBase: new URL("https://www.bloxywebservices.co.uk"),
-  alternates: {
-    canonical: "/",
-    languages: {
-      "en-US": "/en-US",
-      "en-gb": "/en-gb",
-    },
-  },
-  openGraph: {
-    images: "/opengraph-image.png",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "Bloxy Web Services | Freelance Web Designer & Developer 👨‍💻",
+//   description:
+//     "🚀 Supercharge your online presence with incredibly fast websites & apps 🚀 - E-Commerce 🛍️ | Lead Gen 🎯 | Business 🏢 | + more!",
+//   metadataBase: new URL("https://www.bloxywebservices.co.uk"),
+//   alternates: {
+//     canonical: "/",
+//     languages: {
+//       "en-US": "/en-US",
+//       "en-gb": "/en-gb",
+//     },
+//   },
+//   openGraph: {
+//     images: "/opengraph-image.png",
+//   },
+// };
 
-const jsonLd: WithContext<LocalBusiness> = {
-  "@context": "https://schema.org",
-  "@type": "LocalBusiness",
-  "@id": "https://bloxywebservices.co.uk/",
-  name: "Stuart Bloxham - Web Designer & Developer",
-  url: "https://www.bloxywebservices.co.uk/",
-  logo: "https://www.bloxywebservices.co.uk/images/icons/logo.svg",
-  address: {
-    "@type": "PostalAddress",
-    streetAddress: "Hare Street",
-    addressLocality: "Buntingford",
-    addressRegion: "Hertfordshire",
-    addressCountry: "United Kingdom",
-    postalCode: "SG9 0EA",
-  },
-  telephone: "07806615231",
-  email: "stuart@bloxywebservices.co.uk",
-  image: [
-    "https://www.bloxywebservices.co.uk/opengraph-image.png?1581b81fa00074ce",
-    "/screenshot.png",
-  ],
-  openingHoursSpecification: {
-    "@type": "OpeningHoursSpecification",
-    dayOfWeek: [
-      {
-        "@type": "DayOfWeek",
-        name: "Monday",
-      },
-      {
-        "@type": "DayOfWeek",
-        name: "Tuesday",
-      },
-      {
-        "@type": "DayOfWeek",
-        name: "Wednesday",
-      },
-      {
-        "@type": "DayOfWeek",
-        name: "Thursday",
-      },
-      {
-        "@type": "DayOfWeek",
-        name: "Friday",
-      },
-    ],
-    opens: "08:00",
-    closes: "17:30",
-  },
-};
+// const jsonLd: WithContext<LocalBusiness> = {
+//   "@context": "https://schema.org",
+//   "@type": "LocalBusiness",
+//   "@id": "https://bloxywebservices.co.uk/",
+//   name: "Stuart Bloxham - Web Designer & Developer",
+//   url: "https://www.bloxywebservices.co.uk/",
+//   logo: "https://www.bloxywebservices.co.uk/images/icons/logo.svg",
+//   address: {
+//     "@type": "PostalAddress",
+//     streetAddress: "Hare Street",
+//     addressLocality: "Buntingford",
+//     addressRegion: "Hertfordshire",
+//     addressCountry: "United Kingdom",
+//     postalCode: "SG9 0EA",
+//   },
+//   telephone: "07806615231",
+//   email: "stuart@bloxywebservices.co.uk",
+//   image: [
+//     "https://www.bloxywebservices.co.uk/opengraph-image.png?1581b81fa00074ce",
+//     "/screenshot.png",
+//   ],
+//   openingHoursSpecification: {
+//     "@type": "OpeningHoursSpecification",
+//     dayOfWeek: [
+//       {
+//         "@type": "DayOfWeek",
+//         name: "Monday",
+//       },
+//       {
+//         "@type": "DayOfWeek",
+//         name: "Tuesday",
+//       },
+//       {
+//         "@type": "DayOfWeek",
+//         name: "Wednesday",
+//       },
+//       {
+//         "@type": "DayOfWeek",
+//         name: "Thursday",
+//       },
+//       {
+//         "@type": "DayOfWeek",
+//         name: "Friday",
+//       },
+//     ],
+//     opens: "08:00",
+//     closes: "17:30",
+//   },
+// };
 
 export default function RootLayout({
   children,
